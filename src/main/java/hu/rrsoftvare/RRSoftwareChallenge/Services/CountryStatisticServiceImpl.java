c587d88b87e8e294b6b3f0327411297b9a2f82f8
zsolt <WZsolt90@bitbucket.org>
2021-11-25 11:19:13 +0100

    további kommentek

--- src/main/java/hu/rrsoftvare/RRSoftwareChallenge/Services/CountryStatisticServiceImpl.java
+++ src/main/java/hu/rrsoftvare/RRSoftwareChallenge/Services/CountryStatisticServiceImpl.java
@@ -25,6 +25,9 @@ public class CountryStatisticServiceImpl implements CountryStatisticService {
     @Autowired
     DailyStatisticRepository dailyStatisticRepository;
 
+    /**
+     * a kapott statisztikai adatokat ha léteznek felül írja, ha nem akkor betölti az adatbázisba
+     */
     @Override
     public void createOrUpdate(CountryStatDto countryStatDto) {
         Countries country = countryService.getCountryDatasByNameOrIso(countryStatDto.getCountryDto());
@@ -53,6 +56,10 @@ public class CountryStatisticServiceImpl implements CountryStatisticService {
         }
     }
 
+    /**
+     * visszaadja egy adott ország összesített statisztikáját
+     * @return
+     */
     @Override
     public CountryStatDto getCountryStatistic(CountryDto countryDto) {
 
@@ -61,45 +68,74 @@ public class CountryStatisticServiceImpl implements CountryStatisticService {
         return createCountryStatDto(dailyStatisticObj, null, countryService.getCountryDatasByNameOrIso(countryDto));
     }
 
+    /**
+     * a az adott dátumot a nap kezdetére állítja
+     */
     public static Date atStartOfDay(Date date) {
         LocalDateTime localDateTime = dateToLocalDateTime(date);
         LocalDateTime startOfDay = localDateTime.with(LocalTime.MIN);
         return localDateTimeToDate(startOfDay);
     }
 
+    /**
+     * a az adott dátumot a nap végére állítja
+     */
     public static Date atEndOfDay(Date date) {
         LocalDateTime localDateTime = dateToLocalDateTime(date);
         LocalDateTime endOfDay = localDateTime.with(LocalTime.MAX);
         return localDateTimeToDate(endOfDay);
     }
+
+    /**
+     * Date obijektumból LocalDateTime-t készít
+     */
     private static LocalDateTime dateToLocalDateTime(Date date) {
         return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
+    /**
+     * LocalDateTime obijektumból Date-t készít
+     */
     private static Date localDateTimeToDate(LocalDateTime localDateTime) {
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
+    /**
+     * Dátum alapján megkeresi az adott ország statisztikáját
+     */
     @Override
     public CountryStatDto getCountryStatisticByDate(CountryDto countryDto) {
         return getCountryStatisticByDate(countryService.getCountryDatasByNameOrIso(countryDto), countryDto.getSelectedDay());
     }
-    
+
+    /**
+     * ország entitás és dátum alapján visszaadja a statisztikát. ha nem adunk meg dátumot akkor az adott napi statisztikát adja vissza
+     */
     public CountryStatDto getCountryStatisticByDate(Countries country, Date day) {
         return mapEntityToDto(getDailyStatisticByDate(country, day == null ? new Date(): day));
     }
+
+    /**
+     * ország entitás és egy időintervallum között (nap kezdete-nap vége) visszaadja a statisztikát
+     */
     public DailyStatistic getDailyStatisticByDate(Countries country, Date day){
         Date dayStart = atStartOfDay(day);
         Date dayEnd = atEndOfDay(day);
         return dailyStatisticRepository.findByCountryAndDayBetween(country, dayStart, dayEnd);
     }
 
+    /**
+     * visszaadja a globális statisztikát
+     */
     @Override
     public CountryStatDto getGlobalStatistic() {
         SumStat dailyStatisticObj = dailyStatisticRepository.getSummData_Named();
         return createCountryStatDto(dailyStatisticObj, null, null);
     }
 
+    /**
+     * visszaadja az egy régióra vonatkozó statisztikát
+     */
     @Override
     public CountryStatDto getRegionStatistic(String region) {
         SumStat dailyStatisticObj = dailyStatisticRepository.getSummDataByRegion_Named(region);
@@ -107,6 +143,9 @@ public class CountryStatisticServiceImpl implements CountryStatisticService {
 
     }
 
+    /**
+     * modelből csinál dto-t
+     */
     private CountryStatDto mapEntityToDto (DailyStatistic dailyStatistic){
         return new CountryStatDto(dailyStatistic.getTesting(), dailyStatistic.getNewInfected(),
                 dailyStatistic.getDeaths(), dailyStatistic.getHealing(),
@@ -115,6 +154,9 @@ public class CountryStatisticServiceImpl implements CountryStatisticService {
 
     }
 
+    /**
+     * összerakja a statisztika válasz dto-át
+     */
     private CountryStatDto createCountryStatDto(SumStat stat, String region, Countries country){
         CountryStatDto dailyStatistic = new CountryStatDto();
         dailyStatistic.setRegion(region);
